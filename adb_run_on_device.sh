c50411777ecb1482f29b73be2f8d855dfb44fb61
Jiang <lijiangpku@gmail.com>
2022-10-12 16:49:00 -0700

    more image

--- adb_run_on_device.sh
+++ adb_run_on_device.sh
@@ -2,6 +2,8 @@
 set -e
 
 DEVICE_PATH=/data/local/tmp/opengl-es-test
+
+# EXE=01_render_to_image
 EXE=02_render_to_image_pbo
 
 adb shell mkdir -p ${DEVICE_PATH}
@@ -9,7 +11,7 @@ adb push build/bin/${EXE} ${DEVICE_PATH}
 adb shell chmod +x ${DEVICE_PATH}/${EXE}
 adb shell "
 cd ${DEVICE_PATH}
-./${EXE} images/toucan_1280x960.png
+./${EXE} images/toucan_1920x1080.png
 exit
 "
 adb pull ${DEVICE_PATH}/results/img_out.png tmp/
